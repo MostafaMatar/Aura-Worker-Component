8de0a0c2a3ed0b27f36cc0f1be682655baa37a68
Mostafa Matar <moustafaahmed44@yahoo.com>
2019-05-24 18:25:30 +0200

    UserNotifier Helper Component

---
+++ src/aura/UserNotifier/UserNotifierController.js
@@ -0,0 +1,19 @@
+({
+    notifyUser : function(component, event, helper) {
+        var notification = component.get("notification");
+        var toastEvent = $A.get("e.force:showToast");
+        if(notification){
+            if(toastEvent){
+                toastEvent.setParams({
+                    "title": notification.title,
+                    "message": notification.message,
+                    "type": notification.type
+                });
+                toastEvent.fire();
+            }else{
+                alert(notification.message);
+            }
+        }
+
+    }
+})
